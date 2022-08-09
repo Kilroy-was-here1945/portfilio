879d299998268040922d3307e2179ea5a09fd44c
Dallin Johnson <dallin.coder@gmail.com>
2022-08-09 15:33:24 -0600

    .

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express()
 app.get("/", function(res, req) {
     res.setEncoding("working!!!")
 })
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
 
 app.listen(process.env.PORT || 5000);
